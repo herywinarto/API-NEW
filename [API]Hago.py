2ec0896910fc280826a57333fd609307f81b8f73
herywinarto <hery.winarto@binus.ac.id>
2020-06-19 20:05:03 +0700

    Create [API]Hago.py

---
+++ [API]Hago.py
@@ -0,0 +1,10 @@
+import requests, json
+
+def HagoUserID():
+    apiKey = input("Your API key: ")
+    userId = input("Your Hago User ID: ")
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/tiktok?userid="+userId,headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+HagoUserID()
