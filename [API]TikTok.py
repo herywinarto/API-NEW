036f9c79dea4d54b29d09e094946250ae876f5f2
herywinarto <hery.winarto@binus.ac.id>
2020-06-19 20:05:33 +0700

    Update [API]TikTok.py

--- [API]TikTok.py
+++ [API]TikTok.py
@@ -7,11 +7,11 @@ def TiktokUserID():
     main = json.loads(requests.get("https://api.be-team.me/tiktok?userid="+userId,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
-def TiktokUserID():
+def TiktokURL():
     apiKey = input("Your API key: ")
     url = input("Your TikTok URL: ")
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
-    main = json.loads(requests.get("https://api.be-team.me/tiktok?userid="+url,headers=headers).text)
+    main = json.loads(requests.get("https://api.be-team.me/tiktok?url="+url,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
 TiktokUserID()
