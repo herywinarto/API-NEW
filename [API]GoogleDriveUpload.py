faf357e4ce22010060cfcb1d469cd7304dc470b6
herywinarto <hery.winarto@binus.ac.id>
2020-06-19 19:47:24 +0700

    Create [API]GoogleDriveUpload.py

---
+++ [API]GoogleDriveUpload.py
@@ -0,0 +1,9 @@
+def GoogleDriveUpload():
+    apiKey = input("Your API key: ")
+    path = input("Your File Path: ")
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    files = {"file": open(path,"rb")}
+    main = json.loads(requests.post("https://api.be-team.me/gdrive",headers=headers,files=files).text)
+    print("Link: "+ main["result"])
+
+GoogleDriveUpload()
