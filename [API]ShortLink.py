95567846639d43389f6c25763befe597ce181bdc
herywinarto <hery.winarto@binus.ac.id>
2020-06-19 19:44:23 +0700

    Create [API]ShortLink.py

---
+++ [API]ShortLink.py
@@ -0,0 +1,9 @@
+def shorterLink():
+    apiKey = input("Your API key: ")
+    url = input("Your URL: ")
+    headers = {"apiKey": apiKey}
+    data = {"url": url}
+    main = json.loads(requests.post("https://api.be-team.me/short_link",headers=headers, data=data).text)
+    print("Link: "+ main["result"])
+
+shorterLink()
