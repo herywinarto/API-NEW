666457128ff66ea89ebf785000a5eadb116314ed
herywinarto <hery.winarto@binus.ac.id>
2020-06-20 09:29:03 +0700

    Update [API]Hago.py

--- [API]Hago.py
+++ [API]Hago.py
@@ -4,7 +4,7 @@ def HagoUserID():
     apiKey = input("Your API key: ")
     userId = input("Your Hago User ID: ")
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
-    main = json.loads(requests.get("https://api.be-team.me/tiktok?userid="+userId,headers=headers).text)
+    main = json.loads(requests.get("https://api.be-team.me/hago?userid="+userId,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
 HagoUserID()
