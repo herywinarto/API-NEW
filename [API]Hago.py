1a9f12e4cf7ffe6d07f13c7291a8e7db53a61440
herywinarto <hery.winarto@binus.ac.id>
2020-07-04 23:54:48 +0700

    Update [API]Hago.py

--- [API]Hago.py
+++ [API]Hago.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def HagoUserID():
-    apiKey = input("Your API key: ")
-    userId = input("Your Hago User ID: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    userId = "hery" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/hago?userid="+userId,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
