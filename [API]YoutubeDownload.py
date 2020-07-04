c3eb5dd93a90073c9cc46a23a360b3619d55d04d
herywinarto <hery.winarto@binus.ac.id>
2020-07-05 00:06:34 +0700

    Update [API]YoutubeDownload.py

--- [API]YoutubeDownload.py
+++ [API]YoutubeDownload.py
@@ -1,15 +1,15 @@
 import requests, json
 
 def YoutubeSearch():
-    apiKey = input("Your API key: ")
-    search = input("Your Search Query: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    search = "bring me the horizon" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/ytdl?search="+search,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
 def YoutubeURL():
-    apiKey = input("Your API key: ")
-    url = input("Your TikTok URL: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    url = "https://www.youtube.com/watch?v=TkV5709EG5M" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/ytdl?url="+url,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
