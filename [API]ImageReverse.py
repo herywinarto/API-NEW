abc09f3a2db94c3a5dc2bf37928f1f3d3b0b41e9
herywinarto <hery.winarto@binus.ac.id>
2020-07-04 23:55:16 +0700

    Update [API]ImageReverse.py

--- [API]ImageReverse.py
+++ [API]ImageReverse.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def ImageReverse():
-    apiKey = input("Your API key: ")
-    path = input("Your Image Path: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    path = "./img.jgp" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     files = {"file": open(path,"rb")}
     main = json.loads(requests.post("https://api.be-team.me/imgreverse",headers=headers,files=files).text)
