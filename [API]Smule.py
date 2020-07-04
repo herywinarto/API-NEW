7832cc01f032d363b5c887cc65975bc8d3e37472
herywinarto <hery.winarto@binus.ac.id>
2020-07-05 00:02:56 +0700

    Update [API]Smule.py

--- [API]Smule.py
+++ [API]Smule.py
@@ -1,15 +1,15 @@
 import requests, json
 
 def SmuleUserID():
-    apiKey = input("Your API key: ")
+    apiKey = "YOUR_APIKEY_HERE"
     userId = input("Your User ID: ")
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/smule?userid="+userId,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
 def SmuleURL():
-    apiKey = input("Your API key: ")
-    url = input("Your Smule URL: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    url = "https://www.smule.com/c/1112691877_3200708124"
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/smule?url="+url,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
