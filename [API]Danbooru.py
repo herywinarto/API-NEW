ab720cacaae6fdc68cb45f4af89bf396cd008c95
herywinarto <hery.winarto@binus.ac.id>
2020-07-04 23:52:59 +0700

    Update [API]Danbooru.py

--- [API]Danbooru.py
+++ [API]Danbooru.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def DanbooruPage():
-    apiKey = input("Your API key: ")
-    page = input("Your Page Number: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    page = "1" ## EXAMPLE PAGE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/danbooru?page="+page,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
