5039bbc266e78289ae5ab29ec418027032562aa8
herywinarto <hery.winarto@binus.ac.id>
2020-07-04 23:56:03 +0700

    Update [API]InstagramPost.py

--- [API]InstagramPost.py
+++ [API]InstagramPost.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def InstaPost():
-    apiKey = input("Your API key: ")
-    url = input("Your InstagramPost URL: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    url = "https://www.instagram.com/p/CCOdaxxhcA6/" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/instapost?url="+url,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
