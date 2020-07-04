3c090eaac29215083b1abff316f37790474584e4
herywinarto <hery.winarto@binus.ac.id>
2020-07-04 23:52:23 +0700

    Update [API]AuthKeyToPrimary.py

--- [API]AuthKeyToPrimary.py
+++ [API]AuthKeyToPrimary.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def AuthKey2Primary():
-    apiKey = input("Your API key: ")
-    authKey = input("Your AuthKey: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    authKey = "1234567890" ## YOUR AUTHKEY
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/authkey2primary?authkey="+authKey,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
