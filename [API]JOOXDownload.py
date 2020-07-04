85ed5df0b4d1cf86ff2aab2388c29cbf2d42a56a
herywinarto <hery.winarto@binus.ac.id>
2020-07-04 23:58:20 +0700

    Update [API]JOOXDownload.py

--- [API]JOOXDownload.py
+++ [API]JOOXDownload.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def JOOXSearch():
-    apiKey = input("Your API key: ")
-    search = input("Your Search Query: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    search = "bring me the horizon"
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/joox?search="+search,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
