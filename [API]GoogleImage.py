5814f010f9b16f4c1125ddbfa3106e30b02d7919
herywinarto <hery.winarto@binus.ac.id>
2020-07-04 23:54:18 +0700

    Update [API]GoogleImage.py

--- [API]GoogleImage.py
+++ [API]GoogleImage.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def GoogleImage():
-    apiKey = input("Your API key: ")
-    search = input("Your Search Query: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    search = "naruto" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/googleimg?search="+search,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
