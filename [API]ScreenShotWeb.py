c48da340462f648976614e3b2a237dc3be802c3f
herywinarto <hery.winarto@binus.ac.id>
2020-07-05 00:01:53 +0700

    Update [API]ScreenShotWeb.py

--- [API]ScreenShotWeb.py
+++ [API]ScreenShotWeb.py
@@ -1,8 +1,8 @@
 import requests, json
 
 def SSWeb():
-    apiKey = input("Your API key: ")
-    url = input("Your URL: ")
+    apiKey = "YOUR_APIKEY_HERE"
+    url = "https://google.com"
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/ssweb?url="+url,headers=headers).text)
     print("Link: "+ main["result"])
