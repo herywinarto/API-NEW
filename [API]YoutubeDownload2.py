d0205037ec35843a6ba419a57039ab78ddfebcbc
herywinarto <hery.winarto@binus.ac.id>
2020-09-02 19:43:27 +0700

    Update [API]YoutubeDownload2.py

--- [API]YoutubeDownload2.py
+++ [API]YoutubeDownload2.py
@@ -4,14 +4,14 @@ def YoutubeSearch():
     apiKey = "YOUR_APIKEY_HERE"
     search = "Nanairo Symphony" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
-    main = json.loads(requests.get("https://api.be-team.me/ytdl?search="+search,headers=headers).text)
+    main = json.loads(requests.get("https://api.be-team.me/ytdl2?search="+search,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
 def YoutubeURL():
     apiKey = "YOUR_APIKEY_HERE"
     url = "https://www.youtube.com/watch?v=TkV5709EG5M" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
-    main = json.loads(requests.get("https://api.be-team.me/ytdl?url="+url,headers=headers).text)
+    main = json.loads(requests.get("https://api.be-team.me/ytdl2?url="+url,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
 
