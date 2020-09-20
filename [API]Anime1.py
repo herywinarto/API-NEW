119f9d082e974421dffab204441a7678ec7b89b5
herywinarto <hery.winarto@binus.ac.id>
2020-09-20 08:51:43 +0700

    Create [API]Anime1.py

---
+++ [API]Anime1.py
@@ -0,0 +1,15 @@
+import requests, json
+
+def AnimeOngoing():
+    apiKey = "YOUR_APIKEY_HERE"
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/anime/ongoing",headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+    
+ def AnimeList():
+    apiKey = "YOUR_APIKEY_HERE"
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/anime/list",headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+AnimeOngoing()
