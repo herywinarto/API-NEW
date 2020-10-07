6cf10dc1cc295f03256d1dd526957739b7d949d3
herywinarto <hery.winarto@binus.ac.id>
2020-10-07 18:16:02 +0700

    Update [API]SimiSimi.py

--- [API]SimiSimi.py
+++ [API]SimiSimi.py
@@ -1,7 +1,7 @@
 import requests, json
 
 def SimiSimi():
-    apiKey = "fordeveloper"
+    apiKey = "YOUR_APIKEY_HERE"
     text = "hey" ## EXAMPLE
     lang = "en" ## ['af', 'al*', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'cx*', 'ch*', 'hr', 'cs', 'da', 'nl', 'en', 'et', 'ph*', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'he', 'hi', 'hu', 'is', 'id', 'it', 'ja', 'kn', 'kk', 'kh*', 'ko', 'ku', 'lv', 'lt', 'mk', 'ms', 'ml', 'mr', 'mn', 'my', 'ne', 'nb', 'as', 'br', 'gn', 'jv', 'or', 'rw', 'zh*', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'rs*', 'si', 'sk', 'sl', 'es', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vn*', 'cy']
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
