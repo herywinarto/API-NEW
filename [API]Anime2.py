a2aa2886a2f66f4a832b047dff6401dfb08b7f02
herywinarto <hery.winarto@binus.ac.id>
2020-10-28 18:37:00 +0700

    Update [API]Anime2.py

--- [API]Anime2.py
+++ [API]Anime2.py
@@ -7,7 +7,7 @@ def AnimeOngoing():
     main = json.loads(requests.get("https://api.be-team.me/anime2/ongoing",headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
     
- def AnimeList():
+def AnimeList():
     apiKey = "YOUR_APIKEY_HERE"
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/anime2/list",headers=headers).text)
