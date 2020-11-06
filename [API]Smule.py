2b1e08e446316dda685e2e27c63ef7b249b252e8
herywinarto <hery.winarto@binus.ac.id>
2020-11-07 01:34:22 +0700

    Update [API]Smule.py

--- [API]Smule.py
+++ [API]Smule.py
@@ -14,5 +14,12 @@ def SmuleURL():
     main = json.loads(requests.get("https://api.be-team.me/smule?url="+url,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
+def SmulePerformance():
+    apiKey = "YOUR_APIKEY_HERE"
+    userId = "JeffKraad" ## EXAMPLE
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/smule?performance="+userId,headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
 
 SmuleURL()
