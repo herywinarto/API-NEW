a06f2ab68baa522a1410c6263aba2d9349a10b3d
herywinarto <hery.winarto@binus.ac.id>
2020-11-08 00:28:44 +0700

    Update [API]AlphaCoders(Wallpaper).py

--- [API]AlphaCoders(Wallpaper).py
+++ [API]AlphaCoders(Wallpaper).py
@@ -4,7 +4,7 @@ def AlphaCoders():
     apiKey = "YOUR_APIKEY_HERE"
     search = "naruto" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
-    main = json.loads(requests.get("https://api.be-team.me/alphacoders?search="+search,headers=headers).text)
+    main = json.loads(requests.get("https://api.be-team.me/alphacoders?page=1&search="+search,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
 
 AlphaCoders()
