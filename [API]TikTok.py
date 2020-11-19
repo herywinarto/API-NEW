07505b0a91733e0bc0f3adda2a94bedbeadc8e39
herywinarto <hery.winarto@binus.ac.id>
2020-11-19 08:14:36 +0700

    Update [API]TikTok.py

--- [API]TikTok.py
+++ [API]TikTok.py
@@ -2,7 +2,7 @@ import requests, json
 
 def TiktokURL():
     apiKey = "YOUR_APIKEY_HERE"
-    url = "https://www.tiktok.com/@neymarjr/video/6845350696078904582" ## EXAMPLE
+    url = "https://www.tiktok.com/@edowardoginting/video/6896459475486526721" ## EXAMPLE
     headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
     main = json.loads(requests.get("https://api.be-team.me/tiktok?url="+url,headers=headers).text)
     print(json.dumps(main, indent=4, sort_keys=True))
