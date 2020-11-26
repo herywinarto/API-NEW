0e36de13da38c6c3085dc2ac73e9038ee9b295bd
herywinarto <hery.winarto@binus.ac.id>
2020-11-26 22:49:11 +0700

    Create [API]SwapFace.py

---
+++ [API]SwapFace.py
@@ -0,0 +1,45 @@
+import requests, json
+
+def swapfaceImage():
+    apiKey = "YOUR_APIKEY_HERE"
+    url = "https://file.be-team.me/test.jpg" ## EXAMPLE SELFIE
+    templateId = "7a07cd2d-2032-49a4-9c42-29004a7a3ff6" ## GETFROM TEMPLATE LIST
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/swapface/image?url="+url+"&id="+templateId,headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+def swapfaceImageListTemplate():
+    apiKey = "YOUR_APIKEY_HERE"
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/swapface/image/list",headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+def swapfaceGif():
+    apiKey = "YOUR_APIKEY_HERE"
+    url = "https://file.be-team.me/test.jpg" ## EXAMPLE SELFIE
+    templateId = "bb44c753-61e1-4674-aaa1-c3081257094f" ## GETFROM TEMPLATE LIST
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/swapface/gif?url="+url+"&id="+templateId,headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+def swapfaceGifListTemplate():
+    apiKey = "YOUR_APIKEY_HERE"
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/swapface/gif/list",headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+def swapfaceVideo():
+    apiKey = "YOUR_APIKEY_HERE"
+    url = "https://file.be-team.me/test.jpg" ## EXAMPLE SELFIE
+    templateId = "5812bb60-7f25-446a-8174-7ab6ae7814b6" ## GETFROM TEMPLATE LIST
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/swapface/video?url="+url+"&id="+templateId,headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+def swapfaceVideoListTemplate():
+    apiKey = "YOUR_APIKEY_HERE"
+    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
+    main = json.loads(requests.get("https://api.be-team.me/swapface/video/list",headers=headers).text)
+    print(json.dumps(main, indent=4, sort_keys=True))
+
+swapfaceGif()
