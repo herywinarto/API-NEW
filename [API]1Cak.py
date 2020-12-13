ce52027983db94fededc69c8f321f5358769aff3
herywinarto <hery.winarto@binus.ac.id>
2020-12-13 11:42:04 +0700

    Delete [API]1Cak.py

--- [API]1Cak.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-
-def oneCakRandom():
-    apiKey = "YOUR_APIKEY_HERE"
-    headers = {"apiKey": apiKey} ## APIKEY, YOU CAN BUY FROM ME IN WHATSAPP: +6289625658302
-    main = json.loads(requests.get("https://api.be-team.me/1cak",headers=headers).text)
-    print(json.dumps(main, indent=4, sort_keys=True))
-
-oneCakRandom()
